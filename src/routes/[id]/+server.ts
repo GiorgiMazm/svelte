fe081e1bede95b351ffbb8f685bf99e53638b034
Giorgi Mazmisvhilli <giorgi.mazmishvili@telefonica.com>
2024-09-07 18:10:51 +0200

    vercel adapter

--- src/routes/[id]/+server.ts
+++
@@ -1,8 +0,0 @@
-import { supabase } from '$lib/db';
-import { json } from '@sveltejs/kit';
-
-export async function DELETE({ params }) {
-	const id = params.id;
-	await supabase.from('todolist').delete().eq('id', id);
-	return json({ status: 200, message: 'Task deleted successfully' });
-}

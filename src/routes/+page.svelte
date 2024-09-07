851b46f9549396c9ab1ecaeef66b5b463180ef82
Giorgi Mazmisvhilli <giorgi.mazmishvili@telefonica.com>
2024-09-07 20:13:31 +0200

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,60 @@
-<div>hh</div>
+<script lang="ts">
+	import { Input, Label, Button } from 'flowbite-svelte';
+
+	export let data;
+	let toDoItem = '';
+	async function loadData() {
+		const res = await fetch('api/');
+		const loadedData = await res.json();
+		data.todoList = loadedData.data;
+	}
+	async function addToList() {
+		await fetch('api/', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				name: toDoItem
+			})
+		});
+		await loadData();
+		toDoItem = '';
+	}
+
+	async function deleteItem(id: number) {
+		await fetch(`api/${id}`, {
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				name: toDoItem
+			})
+		});
+
+		await loadData();
+	}
+</script>
+
+<section>
+	<div class="container m-auto">
+		<h1>About</h1>
+		<div>
+			<Label for="toDoItem">toDo</Label>
+			<Input class="w-1/3" bind:value={toDoItem} name="toDoItem" type="text" />
+
+			<Button color="dark" on:click={addToList}>Add</Button>
+			<div>
+				<ul>
+					{#each data.todoList as toDoItem}
+						<li>
+							{toDoItem.name}
+							<Button on:click={() => deleteItem(toDoItem.id)}>delete</Button>
+						</li>
+					{/each}
+				</ul>
+			</div>
+		</div>
+	</div>
+</section>

65044b5b1ae83d4e21eac6268974688425eeef3d
Giorgi Mazmisvhilli <giorgi.mazmishvili@telefonica.com>
2024-09-07 20:41:21 +0200

    fix

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,10 @@
 import { supabase } from '$lib/db';
 export const ssr = true; // Forces server-side rendering
 
-export async function load() {
+export async function load({ setHeaders }) {
+	setHeaders({
+		'Cache-Control': 'no-store'
+	});
 	const { data } = await supabase.from('todolist').select();
 	return {
 		todoList: data || []

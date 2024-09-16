ecbddb0174ae9ddbe7ce30abd284c7f9f984dd28
Giorgi Mazmisvhilli <giorgi.mazmishvili@telefonica.com>
2024-09-16 09:40:42 +0200

    add dummy login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,31 +1,33 @@
-<script lang="ts">
-	import { Button, Checkbox, Label, Input } from 'flowbite-svelte';
-</script>
-
-<div class="m-auto mt-40 w-2/5 border p-4">
-	<h1 class="text-center text-3xl">Login</h1>
-	<form class="flex flex-col space-y-6" action="/">
-		<h3 class="p-0 text-xl font-medium text-gray-900 dark:text-white">Change Password</h3>
-		<Label class="space-y-2">
-			<span>Your email</span>
-			<Input type="email" name="email" placeholder="name@company.com" required />
-		</Label>
-		<Label class="space-y-2">
-			<span>Your password</span>
-			<Input type="password" name="password" placeholder="•••••" required />
-		</Label>
-		<div class="flex items-start">
-			<Checkbox>Remember me</Checkbox>
-			<a href="/" class="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500"
-				>Forgot password?</a
-			>
-		</div>
-		<Button type="submit" class="w-full1">Sign in</Button>
-		<p class="text-sm font-light text-gray-500 dark:text-gray-400">
-			Don’t have an account yet? <a
-				href="/"
-				class="text-primary-600 dark:text-primary-500 font-medium hover:underline">Sign up</a
-			>
-		</p>
-	</form>
+<div class="flex h-screen items-center justify-center">
+	<div class="w-full max-w-md rounded-lg bg-white p-8 shadow-lg">
+		<h2 class="mb-6 text-2xl font-bold">Login</h2>
+		<form>
+			<div class="mb-4">
+				<label for="email" class="mb-2 block font-bold text-gray-700"> Email </label>
+				<input
+					type="email"
+					id="email"
+					class="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+					placeholder="Enter your email"
+				/>
+			</div>
+			<div class="mb-6">
+				<label for="password" class="mb-2 block font-bold text-gray-700"> Password </label>
+				<input
+					type="password"
+					id="password"
+					class="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
+					placeholder="Enter your password"
+				/>
+			</div>
+			<div class="flex items-center justify-between">
+				<button
+					type="submit"
+					class="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
+				>
+					Sign In
+				</button>
+			</div>
+		</form>
+	</div>
 </div>
